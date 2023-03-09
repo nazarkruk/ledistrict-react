0425de04b312ed0903550925ae77a7f3a1bf2822
Your NameNazar <nazarkruk@icloud.com>
2023-03-09 00:41:50 -0500

    Spliting to reusable components

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,17 +1,15 @@
 import React from "react";
-import Header from "../../components/Header";
-import Carousel from "../../components/Carousel";
 import { About } from "../../components/About";
 import { Restaurants } from "../../components/Restaurants";
 import { ContactUs } from "../../components/ContactUs";
 import { Map } from "../../components/Map";
 import { Footer } from "../../components/Footer";
+import HeroSection from "../../components/HeroSection";
 
 function HomePage() {
   return (
     <div>
-      <Header />
-      <Carousel />
+      <HeroSection />
       <About />
       <Restaurants />
       <ContactUs />
