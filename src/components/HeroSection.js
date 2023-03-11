7be6befb488c3948e64ebec4780fb1683956c4dc
Your NameNazar <nazarkruk@icloud.com>
2023-03-11 00:03:24 -0500

    Trobleshoting captions alignment

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,17 +3,19 @@ import CarouselImg from "../app/assets/img/carousel-1.jpg";
 
 const HeroSection = ({ imgUrl, title, subTitle }) => {
   return (
-    <header className="header-img">
+    <header className="">
       <div
-        className="p-5 d-block  h-600 text-center bg-image"
+        className="p-5 d-block  header-img h-600 text-center bg-image"
         style={{
           backgroundImage: `url(${imgUrl})`,
           backgroundSize: "cover",
+          backgroundPosition: "center",
+          minWidth: "100%",
           height: "600px",
         }}
       >
-        <div className="container carousel-caption ">
-          <div className="row ">
+        <div className="container d-flex flex-column justify-content-center h-100 r ">
+          <div className="row justify-content-center">
             <div className="col-8 caption-bg  py-3 px-0 ">
               <h3>{title}</h3>
               <div className="border-top border-light w-25 mx-auto my-3"></div>
