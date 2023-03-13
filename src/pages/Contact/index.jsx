9d6b04ea3ab30559af04e4e4f916d4a32c9787cf
Your NameNazar <nazarkruk@icloud.com>
2023-03-12 23:58:42 -0400

    Contact Page buils

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { About } from "../../components/About";
+import { Restaurants } from "../../components/Restaurants";
+import { ContactUs } from "../../components/ContactUs";
+import { Map } from "../../components/Map";
+import { Footer } from "../../components/Footer";
+import HeroSection from "../../components/HeroSection";
+import HomeHero from "../../app/assets/img/carousel-1.jpg";
 
-const Contact = () => {
-  return <div className="text-white">Cart</div>;
-};
+function Contact() {
+  return (
+    <div>
+      <HeroSection
+        title="Contact Us"
+        subTitle="FRENCH SAVOIR FAIRE IN THE HEART OF DOWNTOWN NYC"
+        imgUrl={HomeHero}
+      />
+      <About />
+      <Restaurants />
+      <ContactUs />
+      <Map />
+      <Footer />
+    </div>
+  );
+}
 
 export default Contact;
