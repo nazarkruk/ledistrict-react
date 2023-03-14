1ded2f21e3c57f100dcf945d61c46971afddb80d
Your NameNazar <nazarkruk@icloud.com>
2023-03-13 23:47:07 -0400

    Catering and Events Page

--- src/pages/CateringAndEvents/index.jsx
+++ src/pages/CateringAndEvents/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ContactUs } from "../../components/ContactUs";
+import { Map } from "../../components/Map";
+import { Footer } from "../../components/Footer";
+import HeroSection from "../../components/HeroSection";
+import HomeHero from "../../app/assets/img/carousel-1.jpg";
 
-const CateringAndEvents = () => {
-  return <div className="text-white">Catering And Events</div>;
-};
+function CateringAndEvents() {
+  return (
+    <div>
+      <HeroSection
+        title="Catering and Events"
+        subTitle="FRENCH SAVOIR FAIRE IN THE HEART OF DOWNTOWN NYC"
+        imgUrl={HomeHero}
+      />
+      <ContactUs />
+      <Map />
+      <Footer />
+    </div>
+  );
+}
 
 export default CateringAndEvents;
