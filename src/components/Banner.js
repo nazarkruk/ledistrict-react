7183fb3b6075384b0e18d1c491b5b2bb2208cae8
Your NameNazar <nazarkruk@icloud.com>
2023-04-13 22:52:55 -0400

    Banner build

---
+++ src/components/Banner.js
@@ -0,0 +1,33 @@
+import Button from "./elements/Button";
+
+export const Banner = () => {
+  return (
+    <div className="banner w-full md:w-2/3 px-7 mx-auto relative flex items-center-justify-between">
+      <div className="banner-deescription w-full md:w-1/2 p-3">
+        <h2 className="mb-6 text-4xl font-bold text-white">
+          WELCOME TO LE DISTRICT
+        </h2>
+        <p className="font-semibold text-lg text-red-600 py-2">
+          FRENCH SAVOIR FAIRE IN THE HEART OF DOWNTOWN NYC
+        </p>
+        <div className="btn-container">
+          <Button>Order Now</Button>
+          <a
+            href="/menu"
+            className="text-yellow-400 hover:text-yellow-500 font-bold text-decoration-line px-3"
+          >
+            See Menu
+          </a>
+        </div>
+      </div>
+      <div className="banner-image w-full md:w-1/2 p-3 flex justify-end">
+        {/* <Maincarousel /> */}
+        <img
+          src={require("../assets/images/carousel-1.jpg")}
+          alt="banner"
+          className="max-h-95"
+        />
+      </div>
+    </div>
+  );
+};
